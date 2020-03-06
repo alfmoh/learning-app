ae950c454fe6fd465d1494345f92e2df05a84a2b
alfred <alfmohenu@gmail.com>
2020-03-06 22:31:58 +0300

    Initial Commit

---
+++ src/core/components/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+
+export class App extends React.Component {
+  state = {}
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default App;
